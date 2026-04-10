5189d108174fa087c3521cac9b0a83b48062b295
BJP103 <BJP103@pitt.edu>
2026-04-10 12:00:11 -0400

    Home page ui changes

--- TeamTracker/app/index.tsx
+++ TeamTracker/app/index.tsx
@@ -1,13 +1,14 @@
 import { View, Text, Button, Image } from 'react-native';
-import { router } from 'expo-router';
+import { router, Stack } from 'expo-router';
 import { blue } from 'react-native-reanimated/lib/typescript/Colors';
 
 import { styles } from '../styles/styles';
 
 export default function Home() {
   return (
-        
+    
     <View style={styles.container}>
+      <View style={styles.border}>
       <Image 
         source={require('../assets/images/logo.png')}
         style={styles.logo}
@@ -20,6 +21,7 @@ export default function Home() {
         title="Pick Your Team"
         onPress={() => router.push('/screens/teamlist')}
       />
+      </View>
     </View>
   );
 }
--- TeamTracker/styles/styles.ts
+++ TeamTracker/styles/styles.ts
@@ -4,17 +4,28 @@ export const styles = StyleSheet.create({
    container:{ 
         flex: 1,
         alignItems: 'center',
-        paddingTop: 20,
-        backgroundColor: '#1E3A8A',
+        paddingTop: 60,
+        backgroundColor: '#132453',
     },
     title: {
-        fontSize: 45,
-        fontFamily: 'System',
-        fontWeight: '400',
+        fontSize: 75,
+        fontFamily: 'Arial',
+        fontWeight: '100',
+        fontStyle: 'italic',
         color: 'white',
     },
     logo: {
-        width: 120,
-        height: 120,
+        width: 550,
+        height: 350,
     },
+    border:{
+        borderColor:'#1E3A8A',
+        backgroundColor:'#1E3A8A',
+        borderWidth: 5,
+        borderRadius: 25,
+        padding: 10,
+        width: '80%',
+        alignItems:'center',
+
+    }
 });
